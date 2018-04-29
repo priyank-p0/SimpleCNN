221acdd9be3c84856f60d24d34c02c578af224ec
Priyank Patel <pspbot7@gmail.com>
2018-04-30 03:02:23 +0530

    some changes

--- convnet.py
+++ convnet.py
@@ -1,7 +1,7 @@
 
 """The input dimensions (1x32x32)
  The filter size is (4x16x16) where 4 is the number of filters
- The output size is (10x1)
+ The output size is (1x10)
  architecture is (input->conv->relu->fc->output)"""
 
 
